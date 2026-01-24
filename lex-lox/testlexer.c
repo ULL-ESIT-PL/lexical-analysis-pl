0a5fd382eb9463bf774798d5057d24c782e5331a
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-01-24 22:24:48 +0000

    scanner in C for the lox language presented in the "crafting interpreters" book

--- lex-lox/testlexer.c
+++ lex-lox/testlexer.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
 #include "scanner.h"
 
+const char* tokenTypeName(TokenType type) {
+    static const char* names[] = {
+        // Single-character tokens.
+        "LEFT_PAREN",
+        "RIGHT_PAREN",
+        "LEFT_BRACE",
+        "RIGHT_BRACE",
+        "LEFT_SQUARE",
+        "RIGHT_SQUARE",
+        "COLON",
+        "COMMA",
+        "DOT",
+        "MINUS",
+        "PERCENT",
+        "PLUS",
+        "SEMICOLON",
+        "SLASH",
+        "STAR",
+
+        // One or two character tokens.
+        "BANG",
+        "BANG_EQUAL",
+        "EQUAL",
+        "EQUAL_EQUAL",
+        "GREATER",
+        "GREATER_EQUAL",
+        "LESS",
+        "LESS_EQUAL",
+
+        // Literals.
+        "IDENTIFIER",
+        "STRING",
+        "NUMBER",
+
+        // Keywords.
+        "AND",
+        "CLASS",
+        "ELSE",
+        "FALSE",
+        "FOR",
+        "FUN",
+        "IF",
+        "NIL",
+        "OR",
+        "PRINT",
+        "RETURN",
+        "SUPER",
+        "THIS",
+        "TRUE",
+        "VAR",
+        "WHILE",
+
+        "ERROR",
+        "EOF"
+    };
+
+    if (type < 0 || type >= (int)(sizeof(names) / sizeof(names[0]))) {
+        return "UNKNOWN_TOKEN";
+    }
+
+    return names[type];
+}
+
 int main(int argc, char* argv[]) {
 
     char* source = (argc > 1) ? argv[1] : 
@@ -15,8 +78,9 @@ int main(int argc, char* argv[]) {
         Token token = scanToken(&scanner);  // <-- pass pointer
 
         printf(
-            "type: %d lexeme: '%.*s' line: %d\n",
+            "type (number): %d type(name): %s lexeme: '%.*s' line: %d\n",
             token.type,
+            tokenTypeName(token.type),
             token.length,
             token.start,
             token.line
